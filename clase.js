6f5131ff319bc916b43d76c31240c447cd2d046d
mcaponte2 <157814308+mcaponte2@users.noreply.github.com>
2025-04-22 00:43:21 -0500

    taller

---
+++ clase.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Serie = void 0;
+class Serie {
+    constructor(id, name, channel, seasons, description, website, image) {
+        this.id = id;
+        this.name = name;
+        this.channel = channel;
+        this.seasons = seasons;
+        this.description = description;
+        this.website = website;
+        this.image = image;
+    }
+}
+exports.Serie = Serie;
---
+++ main.ts
@@ -0,0 +1,34 @@
+import { Serie } from "./clase.js";
+import { series } from "./data.js";
+
+function renderSeriesInTable(series: Serie[]): void {
+    const tableBody = document.getElementById("series-body");
+    if (!tableBody) return;
+    series.forEach((serie) => {
+      const trElement = document.createElement("tr");
+  
+      trElement.innerHTML = `
+        <td class="fw-bold">${serie.id}</td>
+        <td><a href="${serie.website}" target="_blank">${serie.name}</a></td>
+        <td>${serie.channel}</td>
+        <td>${serie.seasons}</td>
+      `;
+  
+      tableBody.appendChild(trElement);
+    });
+  
+    const totalseasons = series.reduce((total, serie) => total + serie.seasons, 0);
+    const avrg = totalseasons / series.length;
+  
+    const summaryRow = document.createElement("tr");
+    summaryRow.innerHTML = `
+      <td colspan="7" class="text-start fw-bold">Promedio de temporadas: ${avrg.toFixed(2)}</td>
+    `;
+  
+    tableBody.appendChild(summaryRow);
+  }
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    console.log(series);
+    renderSeriesInTable(series);
+  });
